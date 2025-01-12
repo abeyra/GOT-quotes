afa7d12f06dfdb75ae9ce141c4421e360f32be65
andres beyra <andresbeyra@gmail.com>
2025-01-12 00:41:04 -0500

    simplified script.js and pulled houses and character data

--- script.js
+++ script.js
@@ -1,46 +1,33 @@
-const url = 'https://api.gameofthronesquotes.xyz/v1/random';
+const randomQuoteUrl = 'https://api.gameofthronesquotes.xyz/v1/random';
+const houses = "https://api.gameofthronesquotes.xyz/v1/houses";
+const characters = "https://api.gameofthronesquotes.xyz/v1/characters";
 
-axios.get(url).then(response => {
-    console.log('firing');
-
-    const character = response.data.character.name; 
-    const characterQuote = response.data.sentence; 
-
-    let gotContainer = document.querySelector(".got__container"); 
-
-    let gotCharacter = document.createElement("h1"); 
-    gotCharacter.classList.add("got__character"); 
-    gotCharacter.innerText = character; 
-
-    let quoteContainer = document.createElement("p"); 
-    quoteContainer.classList.add("got__quote"); 
-    quoteContainer.innerText = characterQuote; 
-
-    let gotButton = document.createElement("button"); 
-    gotButton.classList.add("got__button"); 
-    gotButton.innerText = "Get New Quote"; 
-
-    gotContainer.appendChild(gotCharacter); 
-    gotContainer.appendChild(quoteContainer);
-    gotContainer.appendChild(gotButton); 
-
-    // Add event listener here after the button is added to the DOM
-    console.log(gotButton)
-    gotButton.addEventListener("click", function(event){
-        axios.get(url).then(response => {
-            console.log('click');
-            let character = response.data.character.name; 
-            let gotCharacter = document.querySelector(".got__character");
-            gotCharacter.innerText = character; 
-
-            let characterQuote = response.data.sentence; 
-            let quoteContainer = document.querySelector(".got__quote");
-            quoteContainer.innerText = characterQuote;
+try {
+    axios.get(randomQuoteUrl).then(response => {
+        // console.log("random quote path response", response);
+        const character = response.data.character.name; 
+        const characterQuote = response.data.sentence; 
+    
+        document.querySelector(".got__character").innerText = character; 
+        document.querySelector(".got__quote").innerText = characterQuote;
+        let gotButton = document.querySelector(".got__button"); 
+    
+        gotButton.addEventListener("click", function(event){
+            axios.get(randomQuoteUrl).then(response => {
+    
+                let character = response.data.character.name; 
+                let gotCharacter = document.querySelector(".got__character");
+                gotCharacter.innerText = character; 
+    
+                let characterQuote = response.data.sentence; 
+                let quoteContainer = document.querySelector(".got__quote");
+                quoteContainer.innerText = characterQuote;
+            });
         });
-    });
+    
+    })
+}
 
-})
-
-.catch(error => {
+catch{
     console.error('Error fetching the data:', error);
-});
+};
